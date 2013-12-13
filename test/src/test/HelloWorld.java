888bea952abae4efb4da569869f7b00abc72d13c
shayankui <shayankui@126.com>
2013-12-14 02:32:18 +0800

    111

--- test/src/test/HelloWorld.java
+++ test/src/test/HelloWorld.java
@@ -5,6 +5,11 @@ public class HelloWorld {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Hello world.");
+		System.out.println("Hello GitHub.");
+		System.out.println("Hello GitHub.");
+		
+		
+		
 	}
 
 }
